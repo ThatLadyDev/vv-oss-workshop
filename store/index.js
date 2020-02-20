d84e1c6766f1d7efe8e7298562730f37701d6e67
ThatLadyDev <loisbassey@gmail.com>
2020-02-20 11:53:37 +0100

    Made modifications to the index.js file found in the store folder of the project's root directory

--- store/index.js
+++ store/index.js
@@ -1,13 +1,13 @@
 export const state = () => ({
   profiles: [
     {
-      name: 'Gift Egwuenu',
+      name: 'Lois Bassey',
       image:
-        'https://res.cloudinary.com/lauragift/image/upload/w_300,h_300,q_auto/v1562153429/gift5_irlhir.jpg',
-      role: 'Frontend Engineer',
-      Zodiac: 'Taurus',
-      twitter: 'https://twitter.com/lauragift_',
-      linkedin: 'https://www.linkedin.com/in/egwuenugift/'
+        'https://res.cloudinary.com/xxsavage/image/upload/v1582195176/kvngeddieklicks-lois.jpg',
+      role: 'Backend Engineer',
+      Zodiac: 'None',
+      twitter: 'https://twitter.com/That_lady_dev',
+      linkedin: 'https://www.linkedin.com/in/lois-bassey-291870174/'
     }
   ]
 })
